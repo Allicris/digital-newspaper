88171b5371304b4c006bd4c2f2f202ecfc82cfa4
Allison Serrano <allisonserrano171@yahoo.com>
2023-05-31 19:58:08 -0400

    updated new api code to be able to show different gifs

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function getApi() {
     .then(function (response) {
       return response.json();
     })
+    
     .then(function (data) {
       console.log(data)
 
@@ -22,22 +23,37 @@ function getApi() {
         link.textContent = data.response.docs[i].headline.main;
         link.href = data.response.docs[i].web_url;
         tableData.appendChild(link);
+        var keyWord = data.response.docs[i].keywords[0].value;
         createTableRow.appendChild(tableData);
         tableBody.appendChild(createTableRow);
-        getGiphyAPI(data.response.docs[i].abstract);
+        //console.log(data);
+        getGiphyAPI(keyWord,createTableRow);
+        console.log(keyWord);
       }
     });
 }
 fetchButton.addEventListener('click', getApi);
 
-function getGiphyAPI(abstract) {
+
+function getGiphyAPI(abstract, createTableRow) {
+  //var {
+  //   headline  
+  //} = abstract
+//console.log(headline);
   var url = `https://api.giphy.com/v1/gifs/search?api_key=Mvh8fJI89ojteXjhMds1xNInhE9z4zCT&q=${abstract}&limit=1&offset=0&rating=g&lang=en`;
   fetch(url)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
+      //console.log(data);
+     // var reducedAbstract = data.data[0].title.substring(0, 20);
+      //console.log(reducedAbstract);
+      var image = document.createElement("img");
+      image.src = data.data[0].images.fixed_height.url;
+      image.style.maxWidth = "200px";
+      image.style.maxHeight = "200px";
+      createTableRow.appendChild(image);
     })
   }
 
