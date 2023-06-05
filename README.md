64301d60a54d02201a5a115496751c9ee0ea11e4
akash301994 <patel.akash30@icloud.com>
2023-06-05 16:25:13 -0400

    added images folder and finished add favorites

--- README.md
+++ README.md
@@ -1,29 +1,35 @@
 # Digital Newspaper
 
 ## User Story
-As a user, I want to be able to type in a date and have articles be presented to me from any point in time, including the present.
+* As a user, I want to be able to type in a date and have articles be presented to me from any point in time, including the present.
 
-When a date is entered, I want a GIF that relates to the article to be displayed on a loop, until I navigate away from the page.
+* When a date is entered, I want a GIF that relates to the article to be displayed on a loop, until I navigate away from the page.
 
-So that my learning experience can be more enjoyable and research can be more efficient in a rapidly-changing, modern news era.
+* So that my learning experience can be more enjoyable and research can be more efficient in a rapidly-changing, modern news era.
 
 ## Technologies
-Bulma CSS Framework
-New York Times Articles API
-Giphy API
+* Bulma CSS Framework
+* New York Times Articles API
+* Giphy API
 
 ## Teamwork
-We decided that we wanted to work together early on due our shared opinion of not individually feeling confident in our skills. We worked together in class as a team. There were times where we were the last ones in the class and we also met outside of class via Zoom. 
+* We decided that we wanted to work together early on due our shared opinion of not individually feeling confident in our skills. We worked together in class as a team. There were times where we were the last ones in the class and we also met outside of class via Zoom. 
 
 ## Successes & Challenges
-One of our successes and challenges was getting our Giphy API to match with the headlines of the articles. We figured this out by creating a variable that slices the characters on the headline data property and applying it to the Giphy API function. 
+* One of our successes and challenges was getting our Giphy API to match with the headlines of the articles. We figured this out by creating a variable that slices the characters on the headline data property and applying it to the Giphy API function. 
 Our second success and challenge was figuring out how to use a new CSS framework to create a clean website. 
 
 ## Directions for Future Deployment
-Add a different API so the user can have more resources available to them.
-Add option to email links to users for ease of access and sharing options.
-Add filters for gif searches to refine the GIF searches, making them more relevant.
+* Add a different API so the user can have more resources available to them.
+* Add option to email links to users for ease of access and sharing options.
+* Add filters for gif searches to refine the GIF searches, making them more relevant.
 
 ## Credits
-We couldn't have completed this project without the resources that we were given from the bootcamp and the knowledge that we have gained from being taught by Rommel and Ringo. We also recieved help from Rutgers bootcamps tutors and substitute TA's. 
+* We couldn't have completed this project without the resources that we were given from the bootcamp and the knowledge that we have gained from being taught by Rommel and Ringo. We also recieved help from Rutgers bootcamps tutors and substitute TA's. 
+
+# Screenshots of Website
+![APP SCREENSHOT](./images/ss1.png)
+
+![APP SCREENSHOT](./images/ss3.png)
+
 
--- script.js
+++ script.js
@@ -1,40 +1,54 @@
-
-
-//API Call - NY Times
-var tableBody = document.getElementById('repo-table');
-var fetchButton = document.getElementById('fetch-button');
+// //API Call - NY Times
+var tableBody = document.getElementById("repo-table");
+var fetchButton = document.getElementById("fetch-button");
 function getApi() {
-  tableBody.innerHTML = '';
-  var input = document.getElementById('start');
+  tableBody.innerHTML = "";
+  var input = document.getElementById("start");
   var date = input.value;
-  var requestUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=pQodcmInL5JLGLqj8JrP6mVzGiqtek2K&fq=pub_date:(' + date + ')';
+  var requestUrl =
+    "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=pQodcmInL5JLGLqj8JrP6mVzGiqtek2K&fq=pub_date:(" +
+    date +
+    ")";
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
-    
+
     .then(function (data) {
-      console.log(data)
+      console.log(data);
 
       for (var i = 0; i < data.response.docs.length; i++) {
-        var createTableRow = document.createElement('tr');
-        var tableData = document.createElement('td');
-        var link = document.createElement('a');
+        var createTableRow = document.createElement("tr");
+        var tableData = document.createElement("td");
+        var link = document.createElement("a");
         link.textContent = data.response.docs[i].headline.main;
         link.href = data.response.docs[i].web_url;
+        //this opens links in new tab
+        link.target = "_blank";
         tableData.appendChild(link);
-var slicedData = data.response.docs[i].headline.main .substring(0, 30);
+        var slicedData = data.response.docs[i].headline.main.substring(0, 30);
         createTableRow.appendChild(tableData);
         tableBody.appendChild(createTableRow);
         getGiphyAPI(slicedData, createTableRow);
         console.log(slicedData);
+
+        
+
+        // add favorites button
+        var favorite = document.createElement("button");
+        favorite.setAttribute("data-link", data.response.docs[i].web_url);
+        favorite.textContent = "Add to favorites";
+        favorite.onclick = addFavorite;
+        var br = document.createElement("br");
+        tableData.append(br, favorite);
       }
     });
 }
-fetchButton.addEventListener('click', getApi);
+fetchButton.addEventListener("click", getApi);
 
+// get giphy Api call
 
-function getGiphyAPI(abstract, createTableRow) {
+function getGiphyAPI(abstract, createTableRow, favorite) {
   var url = `https://api.giphy.com/v1/gifs/search?api_key=Mvh8fJI89ojteXjhMds1xNInhE9z4zCT&q=${abstract}&limit=1&offset=0&rating=g&lang=en`;
   fetch(url)
     .then(function (response) {
@@ -46,16 +60,34 @@ function getGiphyAPI(abstract, createTableRow) {
       image.style.maxWidth = "200px";
       image.style.maxHeight = "200px";
       createTableRow.appendChild(image);
-    })
-  }
+    });
+}
 
+// this function adds favorited articles to favorites div from local storage. 
 
+function addFavorite() {
+  
+  console.log("clicked");
+  console.log(this);
+  var favorites = JSON.parse(localStorage.getItem("favorited")) || [];
+  var obj = {
+    title: this.dataset.link,
+    
+  };
+  console.log(obj);
+  favorites.push(obj);
+  localStorage.setItem("favorited", JSON.stringify(favorites));
+  this.disabled = true;
+  
+ 
+  var favoritesDiv = document.getElementById('favorites-table');
+  var linkElement = document.createElement('a');
+  var newRow = favoritesDiv.insertRow();
+  var newCell = newRow.insertCell();
+  newCell.textContent = obj.link;
+  linkElement.href = obj.link;
+  linkElement.textContent = obj.title;
+  favoritesDiv.appendChild(linkElement);
 
-
-
-
-
-
-
-
+};
 
